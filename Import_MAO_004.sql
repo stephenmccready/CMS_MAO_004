59fd4ea89420d7fe4bf1cbe7f454a8233daca8e0
Stephen McCready <stephenmccready@gmail.com>
2018-12-11 12:11:00 -0500

    Update Import_MAO_004.sql
    Fixed alignment

--- Import_MAO_004.sql
+++ Import_MAO_004.sql
@@ -23,21 +23,21 @@ Set @today=getDate()
 
 -- Header
 Insert	Into [dbo].[tbl_MAO_004_Header]
-Select	  Cast(SubString(M4.Col001,1,1) As VarChar(1)) As [RecordType]		  -- 0 = Header	
-		, Cast(SubString(M4.Col001,2,1) As VarChar(1)) As [Delimiter01]			    -- *
-		, Cast(SubString(M4.Col001,3,7) As VarChar(7)) As [ReportID]			      -- MAO-004
-		, Cast(SubString(M4.Col001,10,1) As VarChar(1)) As [Delimiter02]		    -- *
-		, Cast(SubString(M4.Col001,11,5) As VarChar(5)) As [MAContractID]		    -- 
-		, Cast(SubString(M4.Col001,16,1) As VarChar(1)) As [Delimiter03]		    -- *
-		, Cast(SubString(M4.Col001,17,8) As VarChar(8)) As [ReportDate]			    -- CCYYMMDD
-		, Cast(SubString(M4.Col001,25,1) As VarChar(1)) As [Delimiter04]		    -- *
-		, Cast(SubString(M4.Col001,26,53) As VarChar(53)) As [ReportDesc]		    -- "Encounter Data Diagnosis Eligible for Risk Adjustment"
-		, Cast(SubString(M4.Col001,79,1) As VarChar(1)) As [Delimiter05]		    -- *
-		, Cast(SubString(M4.Col001,80,30) As VarChar(30)) As [Filler01]			    -- Spaces
-		, Cast(SubString(M4.Col001,110,1) As VarChar(1)) As [Delimiter06]		    -- *
-		, Cast(SubString(M4.Col001,111,4) As VarChar(4)) As [SubmissionFileType]--  PROD or TEST
-		, Cast(SubString(M4.Col001,115,1) As VarChar(1)) As [Delimiter07]		    -- *
-		, Cast(SubString(M4.Col001,116,385) As VarChar(385)) As [Filler02]	    -- Spaces
+Select	  Cast(SubString(M4.Col001,1,1) As VarChar(1)) As [RecordType]		  		-- 0 = Header	
+		, Cast(SubString(M4.Col001,2,1) As VarChar(1)) As [Delimiter01]			-- *
+		, Cast(SubString(M4.Col001,3,7) As VarChar(7)) As [ReportID]			-- MAO-004
+		, Cast(SubString(M4.Col001,10,1) As VarChar(1)) As [Delimiter02]		-- *
+		, Cast(SubString(M4.Col001,11,5) As VarChar(5)) As [MAContractID]		-- 
+		, Cast(SubString(M4.Col001,16,1) As VarChar(1)) As [Delimiter03]		-- *
+		, Cast(SubString(M4.Col001,17,8) As VarChar(8)) As [ReportDate]			-- CCYYMMDD
+		, Cast(SubString(M4.Col001,25,1) As VarChar(1)) As [Delimiter04]		-- *
+		, Cast(SubString(M4.Col001,26,53) As VarChar(53)) As [ReportDesc]		-- "Encounter Data Diagnosis Eligible for Risk Adjustment"
+		, Cast(SubString(M4.Col001,79,1) As VarChar(1)) As [Delimiter05]		-- *
+		, Cast(SubString(M4.Col001,80,30) As VarChar(30)) As [Filler01]			-- Spaces
+		, Cast(SubString(M4.Col001,110,1) As VarChar(1)) As [Delimiter06]		-- *
+		, Cast(SubString(M4.Col001,111,4) As VarChar(4)) As [SubmissionFileType]	--  PROD or TEST
+		, Cast(SubString(M4.Col001,115,1) As VarChar(1)) As [Delimiter07]		-- *
+		, Cast(SubString(M4.Col001,116,385) As VarChar(385)) As [Filler02]	    	-- Spaces
 		, @today As [DateImported]
 		, @filename As [FileName]
 From	[dbo].[tbl_MAO_004_In] As M4
@@ -50,41 +50,41 @@ And		MH.[RecordType] Is Null
 
 -- Detail
 Insert	Into [dbo].[tbl_MAO_004_Detail]
-Select	  Cast(SubString(M4.Col001,1,1) As VarChar(1)) As [RecordType]		  -- 1 = Detail
-		, Cast(SubString(M4.Col001,2,1) As VarChar(1)) As [Delimiter01]				  -- *
-		, Cast(SubString(M4.Col001,3,7) As VarChar(7)) As [ReportID]				    -- MAO-004
-		, Cast(SubString(M4.Col001,10,1) As VarChar(1)) As [Delimiter02]			  -- *
-		, Cast(SubString(M4.Col001,11,5) As VarChar(1)) As [MAContractID]			  -- 
-		, Cast(SubString(M4.Col001,16,1) As VarChar(1)) As [Delimiter03]			  -- *
-		, Cast(SubString(M4.Col001,17,12) As VarChar(12)) As [BeneficiaryHICN]  -- Beneficiary Health Insurance Claim Number
-		, Cast(SubString(M4.Col001,29,1) As VarChar(1)) As [Delimiter04]			  -- *
-		, Cast(SubString(M4.Col001,30,44) As VarChar(44)) As [EncounterICN]		  -- Encounter Data System (EDS) Internal Control Number (ICN)
-																					                                  -- Note: Currently the ICN is 13 characters long
-		, Cast(SubString(M4.Col001,74,1) As VarChar(1)) As [Delimiter05]			  -- *
-		, Cast(SubString(M4.Col001,75,1) As VarChar(1)) As [EncounterTypeSwitch]-- See ## Note 1 Below
-		, Cast(SubString(M4.Col001,76,1) As VarChar(1)) As [Delimiter06]			  -- *
-		, Cast(SubString(M4.Col001,77,44) As VarChar(44)) As [OriginalEncounterICN]-- Encounter Data System (EDS) Internal Control Number (ICN)
-																					-- See ## Note 2 Below
-		, Cast(SubString(M4.Col001,121,1) As VarChar(1)) As [Delimiter07]			  -- *
-		, Cast(SubString(M4.Col001,122,8) As VarChar(8)) As [PlanSubmissionDate]-- CCYYMMDD
-		, Cast(SubString(M4.Col001,130,1) As VarChar(1)) As [Delimiter08]			  -- *
-		, Cast(SubString(M4.Col001,131,8) As VarChar(8)) As [ProcessingDate]	  -- CCYYMMDD
-		, Cast(SubString(M4.Col001,139,1) As VarChar(1)) As [Delimiter09]			  -- *
-		, Cast(SubString(M4.Col001,140,8) As VarChar(8)) As [FromDateOfService] -- CCYYMMDD
-		, Cast(SubString(M4.Col001,148,1) As VarChar(1)) As [Delimiter10]			  -- *
-		, Cast(SubString(M4.Col001,149,8) As VarChar(8)) As [ThruDateOfService]	-- CCYYMMDD
-		, Cast(SubString(M4.Col001,157,1) As VarChar(1)) As [Delimiter11]			  -- *
-		, Cast(SubString(M4.Col001,158,1) As VarChar(1)) As [ClaimType]				  -- Type of Claim: P = Professional, I = Inpatient, O = Outpatient
-		, Cast(SubString(M4.Col001,159,1) As VarChar(1)) As [Delimiter12]			  -- *
-		, Cast(SubString(M4.Col001,160,7) As VarChar(7)) As [DiagnosisCode]			-- ICD-10 (ICD-9 for pre ICD-10 implementation dates)
-		, Cast(SubString(M4.Col001,167,1) As VarChar(1)) As [Delimiter13]			  -- *
-		, Cast(SubString(M4.Col001,168,1) As VarChar(1)) As [DiagnosisICD]			-- 0 = ICD-10, 9 = ICD-9
-		, Cast(SubString(M4.Col001,169,1) As VarChar(1)) As [Delimiter14]			  -- *
+Select	  Cast(SubString(M4.Col001,1,1) As VarChar(1)) As [RecordType]		  		-- 1 = Detail
+		, Cast(SubString(M4.Col001,2,1) As VarChar(1)) As [Delimiter01]			-- *
+		, Cast(SubString(M4.Col001,3,7) As VarChar(7)) As [ReportID]			-- MAO-004
+		, Cast(SubString(M4.Col001,10,1) As VarChar(1)) As [Delimiter02]		-- *
+		, Cast(SubString(M4.Col001,11,5) As VarChar(1)) As [MAContractID]		-- 
+		, Cast(SubString(M4.Col001,16,1) As VarChar(1)) As [Delimiter03]		-- *
+		, Cast(SubString(M4.Col001,17,12) As VarChar(12)) As [BeneficiaryHICN]  	-- Beneficiary Health Insurance Claim Number
+		, Cast(SubString(M4.Col001,29,1) As VarChar(1)) As [Delimiter04]		-- *
+		, Cast(SubString(M4.Col001,30,44) As VarChar(44)) As [EncounterICN]		-- Encounter Data System (EDS) Internal Control Number (ICN)
+									                        -- Note: Currently the ICN is 13 characters long
+		, Cast(SubString(M4.Col001,74,1) As VarChar(1)) As [Delimiter05]		-- *
+		, Cast(SubString(M4.Col001,75,1) As VarChar(1)) As [EncounterTypeSwitch]	-- See ## Note 1 Below
+		, Cast(SubString(M4.Col001,76,1) As VarChar(1)) As [Delimiter06]		-- *
+		, Cast(SubString(M4.Col001,77,44) As VarChar(44)) As [OriginalEncounterICN]	-- Encounter Data System (EDS) Internal Control Number (ICN)
+												-- See ## Note 2 Below
+		, Cast(SubString(M4.Col001,121,1) As VarChar(1)) As [Delimiter07]		-- *
+		, Cast(SubString(M4.Col001,122,8) As VarChar(8)) As [PlanSubmissionDate]	-- CCYYMMDD
+		, Cast(SubString(M4.Col001,130,1) As VarChar(1)) As [Delimiter08]		-- *
+		, Cast(SubString(M4.Col001,131,8) As VarChar(8)) As [ProcessingDate]	  	-- CCYYMMDD
+		, Cast(SubString(M4.Col001,139,1) As VarChar(1)) As [Delimiter09]		-- *
+		, Cast(SubString(M4.Col001,140,8) As VarChar(8)) As [FromDateOfService] 	-- CCYYMMDD
+		, Cast(SubString(M4.Col001,148,1) As VarChar(1)) As [Delimiter10]		-- *
+		, Cast(SubString(M4.Col001,149,8) As VarChar(8)) As [ThruDateOfService]		-- CCYYMMDD
+		, Cast(SubString(M4.Col001,157,1) As VarChar(1)) As [Delimiter11]		-- *
+		, Cast(SubString(M4.Col001,158,1) As VarChar(1)) As [ClaimType]			-- Type of Claim: P = Professional, I = Inpatient, O = Outpatient
+		, Cast(SubString(M4.Col001,159,1) As VarChar(1)) As [Delimiter12]		-- *
+		, Cast(SubString(M4.Col001,160,7) As VarChar(7)) As [DiagnosisCode]		-- ICD-10 (ICD-9 for pre ICD-10 implementation dates)
+		, Cast(SubString(M4.Col001,167,1) As VarChar(1)) As [Delimiter13]		-- *
+		, Cast(SubString(M4.Col001,168,1) As VarChar(1)) As [DiagnosisICD]		-- 0 = ICD-10, 9 = ICD-9
+		, Cast(SubString(M4.Col001,169,1) As VarChar(1)) As [Delimiter14]		-- *
 		, Cast(SubString(M4.Col001,170,1) As VarChar(1)) As [AddOrDeleteFlag]		-- A = Add, D = Delete
-																					-- See ## Note 3 Below
-		, Cast(SubString(M4.Col001,171,1) As VarChar(1)) As [Delimiter15]			  -- *
+												-- See ## Note 3 Below
+		, Cast(SubString(M4.Col001,171,1) As VarChar(1)) As [Delimiter15]		-- *
 		, Cast(SubString(M4.Col001,172,288) As VarChar(288)) As [DiagnosisCodes]	-- See ## Note 4 Below
-		, Cast(SubString(M4.Col001,460,1) As VarChar(1)) As [Filler]				    -- Spaces
+		, Cast(SubString(M4.Col001,460,1) As VarChar(1)) As [Filler]			-- Spaces
 		, @today As [DateImported]
 		, @filename As [FileName]
 From	[dbo].[tbl_MAO_004_In] As M4
@@ -120,15 +120,15 @@ And		MD.[RecordType] Is Null
 
 -- Trailer
 Insert	Into [dbo].[tbl_MAO_004_Trailer]
-Select	  Cast(SubString(M4.Col001,1,1) As VarChar(1)) As [RecordType]		  -- 9 = Trailer
-		, Cast(SubString(M4.Col001,2,1) As VarChar(1)) As [Delimiter01]			    -- *
-		, Cast(SubString(M4.Col001,3,7) As VarChar(7)) As [ReportID]            -- MAO-004
-		, Cast(SubString(M4.Col001,10,1) As VarChar(1)) As [Delimiter02]			  -- *
-		, Cast(SubString(M4.Col001,11,5) As VarChar(1)) As [MAContractID]       -- 
-		, Cast(SubString(M4.Col001,16,1) As VarChar(1)) As [Delimiter03]			  -- *
-		, Cast(SubString(M4.Col001,17,18) As VarChar(18)) As [TotalNumberOfRecords] --
-		, Cast(SubString(M4.Col001,35,1) As VarChar(1)) As [Delimiter04]			  -- *
-		, Cast(SubString(M4.Col001,36,465) As VarChar(465)) As [Filler]			    -- Spaces
+Select	  Cast(SubString(M4.Col001,1,1) As VarChar(1)) As [RecordType]		  		-- 9 = Trailer
+		, Cast(SubString(M4.Col001,2,1) As VarChar(1)) As [Delimiter01]			-- *
+		, Cast(SubString(M4.Col001,3,7) As VarChar(7)) As [ReportID]            	-- MAO-004
+		, Cast(SubString(M4.Col001,10,1) As VarChar(1)) As [Delimiter02]		-- *
+		, Cast(SubString(M4.Col001,11,5) As VarChar(1)) As [MAContractID]       	-- 
+		, Cast(SubString(M4.Col001,16,1) As VarChar(1)) As [Delimiter03]		-- *
+		, Cast(SubString(M4.Col001,17,18) As VarChar(18)) As [TotalNumberOfRecords]	--
+		, Cast(SubString(M4.Col001,35,1) As VarChar(1)) As [Delimiter04]		-- *
+		, Cast(SubString(M4.Col001,36,465) As VarChar(465)) As [Filler]			-- Spaces
 		, @today As [DateImported]
 		, @filename As [FileName]
 From	[dbo].[tbl_MAO_004_In] As M4
